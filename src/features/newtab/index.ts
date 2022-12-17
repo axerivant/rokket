fe6311dc9149db744589797745606f70b85218ef
Axer Ivant <axerivant@icloud.com>
2022-12-17 09:07:07 -0800

    fix manifest

--- src/features/newtab/index.ts
+++ src/features/newtab/index.ts
@@ -1,7 +1,7 @@
 import Newtab from './Newtab.svelte'
 
 const newtab = new Newtab({
-	target: document.getElementById('newtab')
+	target: document.getElementById('newtab-page')
 })
 
 export default newtab
